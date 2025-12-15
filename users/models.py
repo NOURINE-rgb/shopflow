0f0660792616fab863f813bce387e5c53bc79e75
yacine <mohamednourine382@gmail.com>
2025-12-15 17:13:49 +0100

    adding user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class User(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+    points = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+    
