47bf061da991b8185aa46aa532ab35d1d15f70d2
yacine <mohamednourine382@gmail.com>
2026-01-05 15:56:59 +0100

    adding serializers views for products model

--- cart/admin.py
+++ cart/admin.py
@@ -9,6 +9,6 @@ class CartItemInline(admin.TabularInline):
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'created_at', 'updated_at')
-    list_filter = ('created_at')
+    list_filter = ['created_at']
     readonly_fields = ('created_at', 'updated_at')
     inlines = [CartItemInline]
--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import OrderItem
+from .models import Order, OrderItem
 
 # Register your models here.
 class OrderItemInline(admin.TabularInline):
@@ -7,10 +7,10 @@ class OrderItemInline(admin.TabularInline):
     extra = 0
     readonly_fields = ('price',)
     
-@admin.register(OrderItem)
+@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'status', 'total_price', 'created_at')
-    list_filter = ('status', 'created_at')
+    list_filter = ['status', 'created_at']
     search_fields = ('product__name', 'user__username')
     readonly_fields = ('created_at', 'updated_at', 'total_price')
     inlines = [OrderItemInline]
